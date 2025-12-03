95aee44017d7aeb0b1d7cdde709d35d554114b0a
rishabh552 <srishabh2005@gmail.com>
2025-12-03 20:06:20 +0530

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function AppContent() {
       <ModernNavbar />
 
       {/* Routes */}
-      <AnimatePresence mode="wait" initial={false}>
+      <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
